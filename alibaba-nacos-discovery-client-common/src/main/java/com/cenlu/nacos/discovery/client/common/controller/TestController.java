6c780fdf70c5e75be2bb0ca93a7643c908deb9eb
Cenlu <cenluchen@gmail.com>
2019-12-10 16:02:53 +0800

    添加使用Fegin的消费方式

--- alibaba-nacos-discovery-client-common/src/main/java/com/cenlu/nacos/discovery/client/common/controller/TestController.java
+++ alibaba-nacos-discovery-client-common/src/main/java/com/cenlu/nacos/discovery/client/common/controller/TestController.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,15 +18,36 @@ public class TestController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
     @GetMapping("/test")
     public String test(){
         // 通过spring cloud common中的负载均衡接口选取服务提供节点实现接口调用
         String result= restTemplate.getForObject("http://alibaba-nacos-discovery-server/hello?msg=nacos",String.class);
         return  " return: " + result;
     }
+    //使用Feign
+    //1.在pom.xml中增加依赖
+    //2.定义Feign客户端和使用
 
-    @Bean@LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    @Autowired
+    Client client;
+    @GetMapping("/test2")
+    public String test2(){
+        String result = client.hello("nacos");
+        return "Return : "+ result;
     }
+
+    @FeignClient("alibaba-nacos-discovery-server")
+    interface Client{
+
+        @GetMapping("/hello")
+        String hello(@RequestParam(name = "msg") String msg);
+    }
+
+
 }
