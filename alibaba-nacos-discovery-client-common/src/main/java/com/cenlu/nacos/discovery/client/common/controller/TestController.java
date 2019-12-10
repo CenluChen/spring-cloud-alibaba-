18b5c2a498ce61756e1914d550f3d99ad687b171
Cenlu <cenluchen@gmail.com>
2019-12-10 15:45:15 +0800

    使用resttemplate和LoadBalance注解

--- alibaba-nacos-discovery-client-common/src/main/java/com/cenlu/nacos/discovery/client/common/controller/TestController.java
+++ alibaba-nacos-discovery-client-common/src/main/java/com/cenlu/nacos/discovery/client/common/controller/TestController.java
@@ -3,7 +3,9 @@ package com.cenlu.nacos.discovery.client.common.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -12,15 +14,17 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class TestController {
     @Autowired
-    LoadBalancerClient loadBalancerClient;
+    RestTemplate restTemplate;
 
     @GetMapping("/test")
     public String test(){
         // 通过spring cloud common中的负载均衡接口选取服务提供节点实现接口调用
-        ServiceInstance serviceInstance = loadBalancerClient.choose("alibaba-nacos-discovery-server");
-        String url = serviceInstance.getUri() + "/hello?msg=" + "nacos";
-        RestTemplate restTemplate = new RestTemplate();
-        String result= restTemplate.getForObject(url,String.class);
-        return "Invoke :" + url + " ,return: " + result;
+        String result= restTemplate.getForObject("http://alibaba-nacos-discovery-server/hello?msg=nacos",String.class);
+        return  " return: " + result;
+    }
+
+    @Bean@LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
